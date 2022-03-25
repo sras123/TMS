2ccb1a6e5a4858149e065ff338590f5364b2de29
sras123 <np03cs4s210054@heraldcollege.edu.np>
2022-03-25 13:21:25 +0545

    first commit

---
+++ tourmanagementsytem/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+
+# class userInfo(models.Model):
+#     username= models.CharField(max_length=200)
+#     first_name= models.CharField(max_length=200)
+#     last_name= models.CharField(max_length=200)
+#     email= models.EmailField(max_length=254)
+#     password= models.CharField(max_length=200)
+
